1b86e26df6d2e0eb531f0539c8ff3cfcd59f1357
henrimengDEV <henrimeng.developer@gmail.com>
2023-03-16 17:15:48 +0100

    Changes by mickaelaknine

--- src/main/java/use_case/abonnement/SouscrireAbonnement.java
+++ src/main/java/use_case/abonnement/SouscrireAbonnement.java
@@ -1,4 +1,26 @@
 package use_case.abonnement;
 
+import model.abonnement.*;
+import model.abonnement.vo.AbonnementId;
+import model.abonnement.vo.AbonnementStatus;
+
 public class SouscrireAbonnement {
+
+    private final Abonnements abonnements;
+
+    public SouscrireAbonnement(Abonnements abonnements) {
+        this.abonnements = abonnements;
+    }
+
+    public Long souscrireAbonnementClassique(Long compteClientId) {
+
+        Abonnement abonnement = Abonnement
+                .builder()
+                .id(AbonnementId.of(compteClientId))
+                .status(AbonnementStatus.ACTIF)
+                .build();
+
+        Long abonnementId = this.abonnements.save(abonnement);//Shared State ? OUI
+        return abonnementId;     //Shared State ?  NON
+    }
 }
