625e0a25c69495ba3a5b92169637d52a69962886
henrimengDEV <henrimeng.developer@gmail.com>
2023-03-16 17:15:43 +0100

    Changes by krft

--- src/main/java/use_case/abonnement/ResilierAbonnement.java
+++ src/main/java/use_case/abonnement/ResilierAbonnement.java
@@ -1,4 +1,33 @@
 package use_case.abonnement;
 
+import model.abonnement.Abonnement;
+import model.abonnement.Abonnements;
+import model.compte.CompteClient;
+import model.compte.CompteClientId;
+import model.compte.Comptes;
+
 public class ResilierAbonnement {
+
+    private final Abonnements abonnements;
+    private final Comptes comptes;
+
+    public ResilierAbonnement(Abonnements abonnements, Comptes comptes) {
+        this.abonnements = abonnements;
+        this.comptes = comptes;
+    }
+
+    public void resillierAbonnement(CompteClientId compteClientId) throws Exception {
+
+        CompteClient compteClient = comptes.findByCompteClientId(compteClientId);       //Shared State ? OUI
+        Abonnement abonnement = abonnements.findByCompteClientId(compteClient.getId());     //Shared State ? OUI
+
+        abonnement.verifierConditionsResilliation();       //Shared State ? NON
+        compteClient.abonnementActif(false);
+
+        this.abonnements.resilier(abonnement);     //Shared State ? OUI
+        this.comptes.save(compteClient);        //Shared State ? OUI
+
+        comptes.save(compteClient);   //Shared State ? YES
+
+    }
 }
